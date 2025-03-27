d8784dc88f115ae43648095fd908d6e027284fdb
anderrow <anderfercar@gmail.com>
2025-03-27 12:12:12 +0100

    Bulletproof and delete Pointer Overview

--- JsonGenerator.py
+++ JsonGenerator.py
@@ -1,48 +1,55 @@
-#Author: Anderrow 
-#https://github.com/anderrow
+# Author: Anderrow
+# https://github.com/anderrow
 
 import pandas as pd
 import json
 import os
 
 path = "../ProcessViewMessages.xlsm"
-#Read all sheets into a dictionary of DataFrames
-dfs = pd.read_excel(path, sheet_name=None) 
+# Read all sheets into a dictionary of DataFrames
+dfs = pd.read_excel(path, sheet_name=None)
 
-del dfs["ProtonView"] #ProtonView sheet is not needed
+del dfs["ProtonView"]  # ProtonView sheet is not needed
+del dfs["Pointer overview"]  # Pointer Overview is not needed
 
-#--------------------------------FILTER DATAFRAMES--------------------------------#
-df_filtered_dict = {}  #Init
+# --------------------------------FILTER DATAFRAMES--------------------------------#
+df_filtered_dict = {}  # Init
 
 for sheet_name, df in dfs.items():
-    #Save the reference for the df in the dict
+    # Save the reference for the df in the dict
 
-    #Filter dataframe erasing rows that ends with Undefine (Case sensitive is set to false, Nan values are erased)
+    # Replace NaN with an empty string before converting the third column to string
+    df.iloc[:, 2] = df.iloc[:, 2].fillna('')
+
+    # Convert the third column to string to avoid issues with non-string values
+    df.iloc[:, 2] = df.iloc[:, 2].astype(str)
+
+    # Filter dataframe erasing rows that ends with Undefine (Case sensitive is set to false, Nan values are erased)
     df_filtered = df[~df.iloc[:, 2].str.contains(r'.*Undefined$', case=False, na=False)]
-    #Filter dataframe for 'Uknown message'
+
+    # Filter dataframe for 'Unknown message'
     df_filtered2 = df_filtered[~df_filtered.iloc[:, 2].str.contains('Unkown message', case=False, na=False)]
 
-    #Select the second column (Keys) and the 3rd columns (Messages)
+    # Select the second column (Keys) and the third column (Messages)
     df_filtered_subset = df_filtered2.iloc[:, [1, 2]]
 
-    #Erase rows if there is a NaN value (Not message found)
+    # Erase rows if there is a NaN value (Not message found)
     df_cleaned = df_filtered_subset.dropna()
-    
-    # Keep the daframe filtered
+
+    # Keep the dataframe filtered
     df_filtered_dict[sheet_name] = df_cleaned
-    
+
     # Print Before and after
     print("=" * 50)
     print(f"Sheet: {sheet_name}")
     print(f"Original length: {len(df)}")
     print(f"Filtered length: {len(df_cleaned)}")
-    print(f"Data has been reduced by a {100 - len(df_cleaned)*100/len(df):.2f}%")
+    print(f"Data has been reduced by a {100 - len(df_cleaned) * 100 / len(df):.2f}%")
     print("=" * 50)
 
-#--------------------------------END OF FILTER DATAFRAMES--------------------------------#
+# --------------------------------END OF FILTER DATAFRAMES--------------------------------#
 
-
-#--------------------------------GENERATE JSON FILES--------------------------------#
+# --------------------------------GENERATE JSON FILES--------------------------------#
 # Create the folder in the parent directory if it doesn't exist
 output_folder = "../JSON FILES"
 os.makedirs(output_folder, exist_ok=True)
@@ -51,12 +58,12 @@ os.makedirs(output_folder, exist_ok=True)
 for sheet_name, df in df_filtered_dict.items():
     # Generate the name of the file based on the name of the excel sheet
     json_filename = os.path.join(output_folder, f"{sheet_name}.json")
-    
+
     # Convert the df to a dict and save it as a JSON
     df_to_json = df.to_dict(orient='records')
-    
+
     with open(json_filename, 'w') as json_file:
         json.dump(df_to_json, json_file)  # Removed indent parameter for no line breaks
 
     print(f"JSON file {json_filename} has been created.")
-#--------------------------------END OFGENERATE JSON FILESS--------------------------------#
+# --------------------------------END OF GENERATE JSON FILES--------------------------------#

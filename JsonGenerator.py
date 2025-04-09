fb9f0b674f97dd477d21fdde7e778349e9807ef5
anderrow <anderfercar@gmail.com>
2025-04-09 15:12:19 +0200

    Clean UI

--- JsonGenerator.py
+++ JsonGenerator.py
@@ -71,16 +71,12 @@ for sheet_name, df in df_filtered_dict.items():
     # Create the folder in the parent directory if it doesn't exist
     output_folder = f"../JSON FILES/{sheet_name}"
     os.makedirs(output_folder, exist_ok=True)
-    print("*"*50)
+    print(f"\n {sheet_name}")
+    print("*"*(len(f"* JSON file ../JSON FILES/{sheet_name}/aa.json has been created *")))
     
     #Loop to save each column of the dataframe
     for column in range(1, df.shape[1]):
-        column_name = df.columns[column] #Keep name of the column with index column  
-
-        #verify that the column complies with the defined regex pattern
-        if not re.match(column_name_pattern, column_name):
-           raise ValueError(f"\n The column name '{column_name}' doesn't have the requiered format [a-z][a-z]-[A-Z][A-Z]")
-           
+        column_name = df.columns[column] #Keep name of the column with index column             
         column_name_simple = re.sub(r"^([a-zA-Z]+)-.*", r"\1", column_name)
 
         # Generate the name of the file based on the name of the excel sheet
@@ -92,5 +88,7 @@ for sheet_name, df in df_filtered_dict.items():
         with open(json_filename, 'w') as json_file:
             json.dump(df_to_json, json_file)  # Saving without indent for a compact format
 
-        print(f"JSON file {json_filename} has been created")
+        print(f"* JSON file {json_filename} has been created *")
+
+    print("*"*(len(f"* JSON file ../JSON FILES/{sheet_name}/aa.json has been created *")))
 # --------------------------------END OF GENERATE JSON FILES--------------------------------#
